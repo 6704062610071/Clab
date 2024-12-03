987d02b714b21794e7b74b5f52e52f8950b912e7
6704062610071 <s6704062610071@email.kmutnb.ac.th>
2024-12-03 15:34:12 +0700

    ข้อที่3

--- lab1minx.c
+++ lab1minx.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int main() // ข้อที่ 2
+int main() // ข้อที่ 3
 {
     int a;
     int b;
-    scanf("%d", &a);
-    scanf("%d", &b);
+    scanf("%d%d", &a, &b);
+
     if (b > a)
     {
         printf("%d greater than %d\n", b, a);
-        printf("greater than %d",(b-a));
+        printf("greater than %d", (b - a));
     }
     else
     {
         printf("%d greater than %d\n", a, b);
-        printf("greater than %d",(a-b));
+        printf("greater than %d", (a - b));
     }
     return 0;
 }
